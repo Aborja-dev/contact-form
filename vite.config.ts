0dc7be59832c1f46425c1afa576a23a4ab931fc3
ankahara <hecatombe100@gmail.com>
2024-05-31 17:21:55 -0600

    configurar el url base del sitio de deploy y crear el lock.json

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://aborja-dev.github.io/contact-form',
+  base: 'https://aborja-dev.github.io/contact-form/',
   plugins: [react()],
 })
